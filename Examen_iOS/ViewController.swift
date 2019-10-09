05c6f46ef023e381588d5110ea999db25e53ea9d
Freddy Romero Espinosa <freddyromeroespinosa@Freddys-MacBook-Air-3.local>
2019-10-08 21:48:41 -0500

    first commit

---
+++ Examen_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Examen_iOS
+//
+//  Created by Freddy Romero Espinosa on 08/10/19.
+//  Copyright © 2019 Freddy Romero Espinosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
